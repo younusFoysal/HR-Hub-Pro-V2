99c0016f0d4b73e818a289febf35820df2224608
younusFoysal <younus.faysal@gmail.com>
2024-12-30 04:54:26 +0600

    package updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [`jspdf-autotable`],
+    },
+  },
   optimizeDeps: {
     include: ['jquery', 'datatables.net', 'datatables.net-dt']
   },
